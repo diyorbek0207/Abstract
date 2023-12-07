9dc82d88a37e9e6711364e965739150e2555057c
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-07 11:38:47 +0500

    add Cat

--- abs/src/Cat.java
+++ abs/src/Cat.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Cat {
+public class Cat extends Animal{
+    @Override
+    void sound() {
+        System.out.println(cat("meow meow"));
+    }
+
+    @Override
+    void getName() {
+        System.out.println("Koshka");
+    }
+
+    public static void main(String[] args) {
+        Cat cat=new Cat();
+        cat.sound();
+        cat.getName();
+    }
 }
