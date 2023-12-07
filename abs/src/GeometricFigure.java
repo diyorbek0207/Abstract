dd470d030c6442ff4b8994f47872f355b75b6a08
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-07 11:42:01 +0500

    add GeometricFigure

--- abs/src/GeometricFigure.java
+++ abs/src/GeometricFigure.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class GeometricFigure {
+public abstract class GeometricFigure {
+    private int sideA=25;
+    private int sideB=20;
+    private int sideC=23;
+    public GeometricFigure(){
+
+    }
+    public GeometricFigure(int sideA,int sideB,int sideC){
+        this.sideA=sideA;
+        this.sideB=sideB;
+        this.sideC=sideC;
+    }
+    public GeometricFigure(int sideA,int sideB){
+        this.sideA=sideA;
+        this.sideB=sideB;
+    }
+
+    abstract void area();
+    abstract void perimeter();
+
+    public int getSideA(){
+        return sideA;
+    }
+    public int getSideB(){
+        return sideB;
+    }
+    public int getSideC(){
+        return sideC;
+    }
+
 }
