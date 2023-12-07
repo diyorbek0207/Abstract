179aedc1894e1931c8a5aab861eb327e075dd07b
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-07 11:42:36 +0500

    add Rectangle

--- abs/src/Rectangle.java
+++ abs/src/Rectangle.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Rectangle {
+public class Rectangle extends GeometricFigure{
+
+    public Rectangle(int sideA, int sideB) {
+        super(sideA, sideB);
+    }
+    @Override
+    void area() {
+        int S=getSideA() * getSideB();
+        System.out.println(S);
+    }
+
+    @Override
+    void perimeter() {
+        int p=(getSideA() + getSideB())*2;
+        System.out.println(p);
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle=new Rectangle(32,29);
+        rectangle.area();
+        rectangle.perimeter();
+    }
 }
