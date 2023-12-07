c80f23535f36d1175f685c073fbd6522b42c4af8
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-07 11:41:21 +0500

    add Duck

--- abs/src/Duck.java
+++ abs/src/Duck.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Duck {
+public  class Duck extends Animal{
+    @Override
+    void sound() {
+        System.out.println(duck("quack quack"));
+    }
+
+    @Override
+    void getName() {
+        System.out.println("duck");
+    }
+
+    public static void main(String[] args) {
+        Duck duck=new Duck();
+        duck.sound();
+        duck.getName();
+    }
 }
