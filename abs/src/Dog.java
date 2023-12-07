f79b19327c7732ba7db811b9e2a535e174371cef
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-07 11:40:44 +0500

    add Dog

--- abs/src/Dog.java
+++ abs/src/Dog.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Dog {
+public class Dog extends Animal{
+    @Override
+    void sound() {
+        System.out.println(dog("bow bow"));
+    }
+
+    @Override
+    void getName() {
+        System.out.println("Bobik");
+    }
+
+    public static void main(String[] args) {
+        Dog dog=new Dog();
+        dog.sound();
+        dog.getName();
+    }
 }
