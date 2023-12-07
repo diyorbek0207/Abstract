a2f28027d43662597d815cd83f199beb1209a41a
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-07 11:43:54 +0500

    add Trapezium

--- abs/src/Trapezium.java
+++ abs/src/Trapezium.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Trapezium {
+public class Trapezium extends GeometricFigure{
+    private int h=20;
+    public Trapezium(int sideA,int sideB){
+        super (sideA,sideB);
+    }
+    @Override
+    void area() {
+        double S=((getSideA()+getSideB())*h)/2;
+        System.out.println(S);
+    }
+
+    @Override
+    void perimeter() {
+        double p=getSideA()+getSideB()+getSideC()*2;
+        System.out.println(p);
+    }
+
+    public static void main(String[] args) {
+        Trapezium trapezium=new Trapezium(41,36);
+        trapezium.area();
+        trapezium.perimeter();
+    }
 }
