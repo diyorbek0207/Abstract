09fa086edc4fc63c867057f87ad213b98517fa5d
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-07 11:43:19 +0500

    add Tiangle

--- abs/src/Tiangle.java
+++ abs/src/Tiangle.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Tiangle {
+public class Tiangle extends GeometricFigure{
+    public Tiangle(int sideA,int sideB,int sideC){
+        super(sideA,sideB,sideC);
+    }
+    @Override
+    void area() {
+        double p=(getSideA() + getSideB() + getSideC())/2;
+
+        double S=Math.sqrt(p*(p-getSideA())*(p-getSideB())*(p-getSideC()));
+        System.out.println(S);
+    }
+
+    @Override
+    void perimeter() {
+        double p=getSideA() + getSideB() + getSideC();
+        System.out.println(p);
+    }
+
+    public static void main(String[] args) {
+        Tiangle tiangle=new Tiangle(41,35,20);
+        tiangle.area();
+        tiangle.perimeter();
+
+
+    }
 }
